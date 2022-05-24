26efd29c9f6064204de64f0862ceea541d1bf99c
Nelik <akostakioaie.florian.clg@chnu.edu.ua>
2022-05-24 13:40:58 +0300

    starting UI

--- Pages/Levels.xaml.cs
+++ Pages/Levels.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFUIKitProfessional.Pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
